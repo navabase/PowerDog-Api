14886fceebd9c76d67483e5f56e157debe108c49
HannHank <hanke.johann@gmail.com>
2020-05-01 10:03:40 +0200

    added first functions

--- api.py
+++ api.py
@@ -2,29 +2,28 @@
 
 import xmlrpc.client
 import time
-server = xmlrpc.client.ServerProxy('http://api.power-dog.eu/')
 
 class PowerDog():
     def __init__(self,email,md5hash):
         print("Getting API Key")
         print("KEY;", email,md5hash)
         self.server = xmlrpc.client.ServerProxy('http://api.power-dog.eu/')
-        self.apiKey = self.api(email,md5hash)
-        time.sleep(10)
+        self.apiKey = self.api(email,md5hash)['apikey']
+        print("key",self.apiKey)    
 
 	
     def api(self,email,md5hash):
         print("vlaues:",email,md5hash)
         print("server",self.server)
-        response = server.getApiKey(email,md5hash)
+        response = self.server.getApiKey(email,md5hash)
         return response
 
     def getPowerDogs(self):
         response = self.server.getPowerDogs(self.apiKey)
         return response
 
-    def getInverters(self,server,apiKey,PowerDogId):
-        response = server.getInverters(apiKey,PowerDogId)
+    def getInverters(self,PowerDogId):
+        response = self.server.getInverters(self.apiKey,PowerDogId)
         return response
 
     def getSensors(self,PowerDogId):
@@ -35,8 +34,8 @@ class PowerDog():
         response = self.server.getStringData(self.apiKey,sensorId, num, start,end)
         return response
 
-    def getSensorData(self,sensorId, n,start, end):
-        response = self.server.getSensorData(self.apiKey,sensorId,n)
+    def getSensorData(self,sensorId, start, end):
+        response = self.server.getSensorData(self.apiKey,sensorId,start,end)
         return response
 
     def  getPhotovoltaicBorders(self,PowerDogID,month,year):
@@ -47,11 +46,11 @@ class PowerDog():
         response = self.server.getStringDayData(self.apiKey,sensorId, StringNum, day_from, day_to, month, year)
         return response
 
-    def getCounters(self, server, apiKey,PowerDogID):
-        response = server.getCounters(apiKey,PowerDogID)
+    def getCounters(self, PowerDogID):
+        response = self.server.getCounters(self.apiKey,PowerDogID)
         return response
 
-    def getCounterData(self, server, apiKey,counterId,start,end):
-        response = server.getCounterData(apiKey,counterId,start,end)
+    def getCounterData(self, counterId,start,end):
+        response = self.server.getCounterData(self.apiKey,counterId,start,end)
         return response
 
--- main.py
+++ main.py
@@ -1,20 +1,62 @@
 #!flask/bin/python
 from flask import Flask
 from flask import render_template
+from flask import request
+import calendar
+import time
 import api
+import settings
+import os
 
-dog = api.PowerDog("test@mail.com","sfoq3fp3q2jf")
+EMAIL = os.getenv("EMAIL")
+HASH = os.getenv("HASH")
+
+dog = api.PowerDog(EMAIL,HASH)
 
 app = Flask(__name__)
 
+def StartUTConeDay():
+    end = calendar.timegm(time.gmtime())
+    return end - 86400 
 
+def EndUTConeDay():
+    return calendar.timegm(time.gmtime())
 
 @app.route('/')
 def index():
     return render_template('index.html')
-@app.route('/api')
-def Api():
-    return "incomming"
 
+@app.route('/api/v1/<key>/<method>')
+def Api(key,method):
+    print("method",method)
+    if(key == "1234"): # change the password here
+        if(method == "getPowerDogs"):
+            return dog.getPowerDogs()
+        elif (method == "getInverters"):
+            PowerDogId = request.args.get('PowerDogId',default = 1, type = int)
+            return dog.getInverters(PowerDogId)
+
+        elif (method == "getCounters"):
+            PowerDogId = request.args.get('PowerDogId',default = 1, type = int)
+            return dog.getCounters(PowerDogId)
+
+        elif (method == "getSensors"):
+            PowerDogId = request.args.get('PowerDogId',default = 1, type = int)
+            return dog.getSensors(PowerDogId)
+
+        elif (method == "getPhotovoltaicBorders"):
+            PowerDogId = request.args.get('PowerDogId',default = 1, type = int)
+            month = request.args.get('month',default = 2020, type = int)
+            year = request.args.get('year',default = 1, type = int)
+            return dog.getPhotovoltaicBorders(PowerDogId,month,year)
+        elif (method == "getSensorData"):
+            SensorsId = request.args.get('SensorId',default = 0000, type = int)
+            # TODO
+        elif (method == "getCounterData"):
+            CounterId = request.args.get('CounterId',default = 0000, type = int)
+            start = request.args.get('start',default = StartUTConeDay(), type = int)
+            end = request.args.get('end',default = EndUTConeDay(), type = int)
+            return dog.getCounterData(CounterId,start,end)
+    return render_template('notAllowed.html',pwd=key)
 if __name__ == '__main__':
     app.run(debug=True)
